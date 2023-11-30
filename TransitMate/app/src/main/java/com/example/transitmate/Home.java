469d6c08d254bff344498c7282bdc1a1e9b2f156
MithishR <mithishrg@gmail.com>
2023-11-29 18:00:28 -0800

    finished settings page

--- TransitMate/app/src/main/java/com/example/transitmate/Home.java
+++ TransitMate/app/src/main/java/com/example/transitmate/Home.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -13,6 +14,7 @@ import android.content.Intent;
 public class Home extends Fragment {
 
     TextView homeTitle;
+    ImageView imageButton;
 
     public Home(){
         // require a empty public constructor
@@ -23,13 +25,27 @@ public class Home extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         homeTitle = (TextView) view.findViewById(R.id.homeTitle);
+        imageButton = view.findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // This method will be called when the ImageView is clicked
+                launchSettings();
+            }
+        });
+
+
+
 
         return view;
     }
-
-    public void launchSettings(View view) {
-        Intent intent = new Intent(getActivity(), SettingsActivity.class);
+    private void launchSettings() {
+        // Your logic to launch settings goes here
+        // For example, you can start a new activity
+        Intent intent = new Intent(getActivity(), Settings.class);
         startActivity(intent);
     }
-
 }
+
+
+
