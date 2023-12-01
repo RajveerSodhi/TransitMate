8210e560b053ff7f55736776a2567775baf35cf9
MithishR <mithishrg@gmail.com>
2023-11-30 18:33:33 -0800

    Finished all screens for task 1

--- TransitMate/app/src/main/java/com/example/transitmate/TripPassengerFragment.java
+++ TransitMate/app/src/main/java/com/example/transitmate/TripPassengerFragment.java
@@ -1,31 +1,110 @@
 package com.example.transitmate;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.*;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TimePicker;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class TripPassengerFragment extends Fragment {
 
+    private EditText dateEditText;
+    private EditText timeEditText;
 
-    public TripPassengerFragment(){
-        // require a empty public constructor
+    private Calendar selectedDate;
+    private int selectedHour, selectedMinute;
+
+    public TripPassengerFragment() {
+        // require an empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trip_passenger, container, false);
 
+        dateEditText = view.findViewById(R.id.editTextDate);
+        timeEditText = view.findViewById(R.id.editTextTime);
 
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
 
+        timeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
         return view;
     }
 
+    private void showDatePickerDialog() {
+        final Calendar currentDate = Calendar.getInstance();
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        selectedDate = Calendar.getInstance();
+                        selectedDate.set(Calendar.YEAR, year);
+                        selectedDate.set(Calendar.MONTH, month);
+                        selectedDate.set(Calendar.DAY_OF_MONTH, day);
 
+                        updateDateEditText();
+                    }
+                },
+                currentDate.get(Calendar.YEAR),
+                currentDate.get(Calendar.MONTH),
+                currentDate.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
+    private void showTimePickerDialog() {
+        final Calendar currentTime = Calendar.getInstance();
+        int currentHour = currentTime.get(Calendar.HOUR_OF_DAY);
+        int currentMinute = currentTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                requireContext(),
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        selectedHour = hourOfDay;
+                        selectedMinute = minute;
+
+                        updateTimeEditText();
+                    }
+                },
+                currentHour,
+                currentMinute,
+                false // 24-hour format
+        );
+        timePickerDialog.show();
+    }
+
+    private void updateDateEditText() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+        dateEditText.setText(sdf.format(selectedDate.getTime()));
+    }
+
+    private void updateTimeEditText() {
+        String timeFormat = String.format(Locale.US, "%02d:%02d", selectedHour, selectedMinute);
+        timeEditText.setText(timeFormat);
+    }
 }
