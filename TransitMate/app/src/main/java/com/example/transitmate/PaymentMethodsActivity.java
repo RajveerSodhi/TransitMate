7dc34adbdf209d5f4104e97a799cae79d039e66a
MithishR <mithishrg@gmail.com>
2023-12-06 11:43:46 -0800

    App completed

--- TransitMate/app/src/main/java/com/example/transitmate/PaymentMethodsActivity.java
+++ TransitMate/app/src/main/java/com/example/transitmate/PaymentMethodsActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,6 +16,7 @@ public class PaymentMethodsActivity  extends AppCompatActivity {
     TextView tripCostText;
     Bundle drInfo;
     Button paymentButton, backButton;
+    RadioGroup paymentOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +26,40 @@ public class PaymentMethodsActivity  extends AppCompatActivity {
         tripCostText = (TextView) findViewById(R.id.tripCostText);
         paymentButton = (Button) findViewById(R.id.paymentButton);
         backButton = (Button) findViewById(R.id.backButton);
-
+        paymentOptions = findViewById(R.id.paymentOptions);
         drInfo = getIntent().getExtras();
         tripCostText.setText("CAD " + drInfo.getString("cost", "84.89"));
 
         // TODO: don't let user press continue without selecting one of the payment options
 
         paymentButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(PaymentMethodsActivity.this, "Payment Initiated. Trip Confirmed!", Toast.LENGTH_SHORT).show();
+//                Home.tripProgress = true;
+//                Intent goHome = new Intent(PaymentMethodsActivity.this, MainActivity.class);
+//                goHome.putExtras(drInfo);
+//                startActivity(goHome);
+//                Toast.makeText(PaymentMethodsActivity.this, "You can now view Trip Progress info from the Home Screen", Toast.LENGTH_SHORT).show();
+//            }
+
             @Override
             public void onClick(View v) {
-                Toast.makeText(PaymentMethodsActivity.this, "Payment Initiated. Trip Confirmed!", Toast.LENGTH_SHORT).show();
-                Home.tripProgress = true;
-                Intent goHome = new Intent(PaymentMethodsActivity.this, MainActivity.class);
-                goHome.putExtras(drInfo);
-                startActivity(goHome);
-                Toast.makeText(PaymentMethodsActivity.this, "You can now view Trip Progress info from the Home Screen", Toast.LENGTH_SHORT).show();
+                int selectedPaymentId = paymentOptions.getCheckedRadioButtonId();
+
+                if (selectedPaymentId == -1) {
+                    // No payment option selected
+                    Toast.makeText(PaymentMethodsActivity.this, "Please select a payment option", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Payment option selected
+                  //  RadioButton selectedPaymentOption = findViewById(selectedPaymentId);
+                    Toast.makeText(PaymentMethodsActivity.this, "Payment Initiated. Trip Confirmed!", Toast.LENGTH_SHORT).show();
+                    Home.tripProgress = true;
+                    Intent goHome = new Intent(PaymentMethodsActivity.this, MainActivity.class);
+                    goHome.putExtras(drInfo);
+                    startActivity(goHome);
+                    Toast.makeText(PaymentMethodsActivity.this, "You can now view Trip Progress info from the Home Screen", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
--- TransitMate/app/src/main/java/com/example/transitmate/TripProgressActivity.java
+++ TransitMate/app/src/main/java/com/example/transitmate/TripProgressActivity.java
@@ -1,21 +1,50 @@
 package com.example.transitmate;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.Color;
+import android.os.*;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 
-public class TripProgressActivity extends AppCompatActivity {
+import org.json.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
+
+public class TripProgressActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     Button shareButton, rateButton, helpButton;
     ImageButton tripProgBack;
+    Marker originMarker, destMarker;
+
     Bundle tripInfo;
+    GoogleMap gmap;
+
     TextView pickup, destin, price, driver, dandt, eta, distance;
+
+    MapView mapView;
+    Polyline routePolyline;
+    LatLng OL, DL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +63,15 @@ public class TripProgressActivity extends AppCompatActivity {
         eta = (TextView) findViewById(R.id.eta);
         distance = (TextView) findViewById(R.id.distance);
         tripInfo = getIntent().getExtras();
-
-        pickup.setText(tripInfo.getString("pickup"));
-        destin.setText(tripInfo.getString("dest"));
+        OL=new LatLng(49.3732,-121.4419);
+        DL = TripPassengerFragment.destinationLatLng;
+        mapView = (MapView) findViewById(R.id.mapProgress);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+        pickup.setText(TripPassengerFragment.originSearch.getQuery().toString());
+        destin.setText(TripPassengerFragment.destinationSearch.getQuery().toString());
         driver.setText(tripInfo.getString("name"));
-        dandt.setText(tripInfo.getString("dandt"));
+        dandt.setText(TripPassengerFragment.dateEditText.getText().toString());
         price.setText(tripInfo.getString("cost"));
 
         // TODO: set eta, distance
@@ -75,5 +108,114 @@ public class TripProgressActivity extends AppCompatActivity {
             }
         });
 
+    }
+    //CODE REFERRED FROM https://www.digitalocean.com/community/tutorials/android-google-map-drawing-route-two-points
+    // ALSO HELP WAS TAKEN FROM https://developers.google.com/maps/documentation/android-sdk/polygon-tutorial API DEVELOPMENT KIT
+
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        originMarker = gmap.addMarker(new MarkerOptions().position(OL).title("Origin"));
+        destMarker = gmap.addMarker(new MarkerOptions().position(DL).title("Destination"));
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(originMarker.getPosition());
+        builder.include(destMarker.getPosition());
+        LatLngBounds bounds = builder.build();
+        gmap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+
+        String directionsUrl = "https://maps.googleapis.com/maps/api/directions/json" +
+                "?origin=" + OL.latitude+","+OL.longitude +"&destination=" + DL.latitude+","+DL.longitude + "&key=AIzaSyCGzZ4JWj8C2SMqGkvkuCpbZiIj0lzM9QY";
+        new TripProgressActivity.FetchRouteTask().execute(directionsUrl);
+    }
+    private class FetchRouteTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder result = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+                return result.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @SuppressLint("StaticFieldLeak")
+        @Override
+        protected void onPostExecute(String directionsData) {
+            if (directionsData != null) {
+                Log.d("DirectionsData", directionsData);
+                drawRoute(directionsData);
+            }
+        }
+    }
+    private void drawRoute(String directionsData) {
+        try {
+            JSONObject jsonObject = new JSONObject(directionsData);
+            JSONArray routes = jsonObject.getJSONArray("routes");
+
+            if (routes.length() > 0) {
+                JSONObject route = routes.getJSONObject(0);
+                JSONObject overviewPolyline = route.getJSONObject("overview_polyline");
+                String encodedPolyline = overviewPolyline.getString("points");
+                List<LatLng> decodedPolyline = PolyUtil.decode(encodedPolyline);
+                PolylineOptions options = new PolylineOptions()
+                        .addAll(PolyUtil.decode(encodedPolyline))
+                        .color(Color.BLUE)
+                        .width(10);
+                routePolyline = gmap.addPolyline(options);
+
+                routePolyline = gmap.addPolyline(options);
+                for (LatLng point : decodedPolyline) {
+                    Log.d("PolylinePoint", "Lat: " + point.latitude + ", Lng: " + point.longitude);
+                    Log.d("Polyline", "Number of points: " + decodedPolyline.size());
+                }
+
+                // Move camera to show both markers and the route
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                builder.include(originMarker.getPosition());
+                builder.include(destMarker.getPosition());
+                for (LatLng point : decodedPolyline) {
+                    builder.include(point);
+                }
+                LatLngBounds bounds = builder.build();
+                gmap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200));
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
